3a1d128b833e1ceb9129c14d1bbd21013afdf880
github yuusan-lgtm <sbyuu3@gmail.com>
2022-04-15 13:38:53 +0900

    change

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 from collections import OrderedDict
 
-def sorted_by_key(d):
-    return OrderedDict(sorted(d.items(), key=lambda k:k[0]))
+def sorted_by_key(a):
+    return OrderedDict(sorted(a.items(), key=lambda d: d[0]))
